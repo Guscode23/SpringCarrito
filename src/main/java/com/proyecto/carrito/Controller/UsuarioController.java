00c69fe69761dcf0d496ec76e37e9f385c369f29
Gustavo <rosalesunlam19@gmail.com>
2023-03-22 21:33:41 -0300

    Agregando funcionalidades al usuario

--- src/main/java/com/proyecto/carrito/Controller/UsuarioController.java
+++ src/main/java/com/proyecto/carrito/Controller/UsuarioController.java
@@ -4,12 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.proyecto.carrito.model.Cliente;
 import com.proyecto.carrito.service.IUsuarioService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/Cliente")
@@ -34,4 +37,27 @@ public class UsuarioController {
 		usuarioService.save(usuario);
 		return "redirect:/";
 	}
-}
+	
+	@GetMapping("/login")
+	public String login() {
+	    return "Cliente/login";
+	}
+	
+	@PostMapping("/acceder")
+	public String acceder(Cliente usuario) {
+		 logger.info("Accesos:{}",usuario);
+		 
+		
+		return "redirect:/";
+		
+	}
+	
+	@GetMapping("/compras")
+	public String obtenerCompra(Model model,HttpSession session) {
+		model.addAttribute("sesion",session.getAttribute("idusuario"));
+		return "Cliente/compras";
+	}
+	
+	
+	}
+
